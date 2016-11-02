7f79847a50b63e3872eaa56f66418761ddb4d678
John Doe <johndoe@example.com>
2016-11-02 15:36:26 -0700

    Hey so I was able to do this despite Jay's attempts to sabotage my PHP

--- coin.php
+++ coin.php
@@ -1,4 +1,5 @@
 <?php
+echo "Starting the program" . '<br />' . '<br />';
 $heads = 0;
 $tails = 0;
 for ($i=1; $i < 5001; $i++) { 
@@ -12,5 +13,5 @@ for ($i=1; $i < 5001; $i++) {
 		echo "Attempt #" . $i . ": Throwing a coin... It's a head! ... Got " . $heads . " head(s) so far and " . $tails . " tail(s) so far" . "<br />";
 	}
 }
-
+echo '<br />' . '<br />' . "Ending the program - thank you!";
 ?>
