11780d55816eb485311e8574a16af24cffe31337
John Doe <johndoe@example.com>
2016-11-02 15:21:37 -0700

    Hey I hope this works if not this is all Jay's fault AS USUAL

---
+++ coin.php
@@ -0,0 +1,16 @@
+<?php
+$heads = 0;
+$tails = 0;
+for ($i=1; $i < 5001; $i++) { 
+	$coin = rand(1,2);
+	if ($coin == 1) {
+		$heads++;
+		echo "Attempt #" . $i . ": Throwing a coin... It's a head! ... Got " . $heads . " head(s) so far and " . $tails . " tail(s) so far" . "<br />";
+	}
+	else{
+		$tails++;
+		echo "Attempt #" . $i . ": Throwing a coin... It's a head! ... Got " . $heads . " head(s) so far and " . $tails . " tail(s) so far" . "<br />";
+	}
+}
+
+?>
